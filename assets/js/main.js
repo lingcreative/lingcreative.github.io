e2f27b0b1dd64be6015c321781c5fced1ca99457
Sauntor <sauntor@SuSE.os>
2014-10-05 13:01:49 +0800

    remove dom ready handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
-jQuery(function(){
-	var $ = jQuery,
-		selector = "#navbar > .navbar-collapse > ul > li",
-		active = "active";
-	$(selector).click(function(event){
-		$(selector).removeClass(active);
-		$(this).addClass(active);
-	});
-});
+console.info("我们期待您的加入！");
